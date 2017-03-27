e1ebdfe32e3be1743962586dd04b716bae31bdf3
Nenad <ahhhhmed@gmail.com>
2017-03-27 23:50:51 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='homology',
+    version='0.0',
+    packages=['homology', 'test'],
+    url='',
+    license='',
+    author='Nenad',
+    author_email='ahhhhmed@gmail.com',
+    description='snippet engine'
+)
