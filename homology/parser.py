e30e612cf637edde42e00534dfff3bdb6a207eb4
Nenad <ahhhhmed@gmail.com>
2017-05-09 22:37:36 +0200

    Implement parser.

---
+++ homology/parser.py
@@ -0,0 +1,58 @@
+import ply.lex as lex
+import ply.yacc as yacc
+from homology.syntaxtree import SimpleSnippet, CompositeSnippet
+
+#################### Lexer #########################
+
+# List of token names.
+tokens = (
+    'SNIPPET',
+    'LEFT_OPERATOR',
+    'RIGHT_OPERATOR',
+)
+
+# Starting letters of operators.
+left = '!@#'
+right = '$%:'
+
+# Regular expression rules for tokens
+t_SNIPPET = r'[a-zA-Z_0-9]+'
+t_LEFT_OPERATOR = r'[{0}][{0}{1}]*'.format(left, right)
+t_RIGHT_OPERATOR = r'[{1}][{0}{1}]*'.format(left, right)
+
+
+# Error handling rule
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+# Build the lexer
+lexer = lex.lex()
+
+#################### Parser ########################
+
+# Set up precedence.
+precedence = (
+    ('left', 'LEFT_OPERATOR'),
+    ('right', 'RIGHT_OPERATOR'),
+)
+
+
+# Grammar rules.
+def p_expression_plus(p):
+    """
+    expression : expression LEFT_OPERATOR expression
+               | expression RIGHT_OPERATOR expression
+    """
+    p[0] = CompositeSnippet(p[1], p[2], p[3])
+
+
+def p_expression_minus(p):
+    """
+    expression : SNIPPET 
+    """
+    p[0] = SimpleSnippet(p[1])
+
+
+# Build the parser
+parser = yacc.yacc()
--- test/testParser.py
+++ test/testParser.py
@@ -1,4 +1,19 @@
 from unittest import TestCase
 
+from homology.parser import parser
+from homology.syntaxtree import SimpleSnippet, CompositeSnippet
+
+
 class TestParser(TestCase):
-    pass
+    def test_basic(self):
+        self.assertEqual(parser.parse('asd'), SimpleSnippet('asd'))
+
+    def test_left_associativity(self):
+        left = '!@#'
+        for l in left:
+            self.assertEqual(parser.parse('first{0}second{0}third'.format(l)), CompositeSnippet(CompositeSnippet(SimpleSnippet('first'), l, SimpleSnippet('second')), l, SimpleSnippet('third')))
+
+    def test_right_associativity(self):
+        right = '$%:'
+        for r in right:
+            self.assertEqual(parser.parse('first{0}second{0}third'.format(r)), CompositeSnippet(SimpleSnippet('first'), r, CompositeSnippet(SimpleSnippet('second'), r, SimpleSnippet('third'))))
